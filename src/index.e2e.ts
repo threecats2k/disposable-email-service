e656a35d8a1a6ac51df736515b1dc13de6126cb4
threecats2k <threecats2k@gmail.com>
2025-09-20 20:16:53 +0700

    feat: handle keep email and get subject form first mail

--- src/index.e2e.ts
+++ src/index.e2e.ts
@@ -2,6 +2,9 @@ import { DisposableEmailHelper, DisposableServiceOptions } from './index';
 import fetchV2 from './lib/fetch-2.0';
 import { MailServices } from './utils/constant';
 
+
+jest.setTimeout(10 * 60 * 1000); // 10 minutes
+
 describe('DisposableService', () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -32,10 +35,34 @@ describe('DisposableService', () => {
 
   it('should fetch real data  mail 10m', async () => {
     const service = new DisposableEmailHelper({ mailService: MailServices.MAIL10MIN });
-    const email = await service.getMail();
+    const { email }  = await service.getMail();
     console.log(email);
     expect(email).toBeDefined();
     expect(email).toMatch(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+  });
+
+
+  it('should fetch same mail 10m between 2 request', async () => {
+    const service = new DisposableEmailHelper({ mailService: MailServices.MAIL10MIN });
+    const { email, cookie }  = await service.getMail();
+    console.log(email);
+    console.log(cookie);
+    expect(email).toBeDefined();
+    expect(email).toMatch(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+    const { email: email2 }  = await service.getMail({ cookie });  
+    expect(email).toBe(email2);
+  });
+
+  it('should fetch content mail', async () => {
+    const service = new DisposableEmailHelper({ mailService: MailServices.MAIL10MIN });
+    const { email, cookie }  = await service.getMail();
+    console.log(email);
+    console.log(cookie);
+    expect(email).toBeDefined();
+    expect(email).toMatch(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+    const { content }  = await service.getContent(cookie || [], 'spdev1203@gmail.com');
+    console.log(content);
+    expect(content).toBeDefined();
   });
 
 });
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,25 @@ import fetchV2 from './lib/fetch-2.0';
 import { DisposableEmailService } from './types';
 import { DISPOSABLE_EMAIL_SERVICES, MailServices } from './utils/constant';
 import { Headless } from './lib/headless';
+import { CookieData } from 'puppeteer';
+import { OutgoingHttpHeader } from 'http';
 
 export * from './utils/helper';
 export * from './utils/constant';
 export * from './types';
 export * from './lib/fetch-2.0';
 export * from './lib/headless';
+
+export type MailRes = {
+  email: string | null;
+  cookie?: CookieData[];
+}
+
+export type ContentMailRes = {
+  subject: string | null;
+  content: string | null;
+  cookie?: CookieData[];
+}
 
 export type DisposableServiceOptions = {
   /**
@@ -17,6 +30,10 @@ export type DisposableServiceOptions = {
    */
   mailService?: MailServices;
 }
+
+export type CustomHeader = OutgoingHttpHeaders
+  | { cookie?: CookieData[] | undefined }
+  | Record<string, any>;
 
 export class DisposableEmailHelper {
   private _mailSvc: DisposableEmailService;
@@ -28,31 +45,104 @@ export class DisposableEmailHelper {
   /**
    * Get a disposable email address from the default mail service.
    */
-  async getMail(customHeaders: OutgoingHttpHeaders & Record<string, any> = {}): Promise<any> {
-    if (this._mailSvc.headless) return this.useHeadless();
+  async getMail(customHeaders: CustomHeader = {}): Promise<MailRes> {
+    if (this._mailSvc.headless) return this.useHeadless(customHeaders?.['cookie']);
     if (this._mailSvc.httpVersion === '2') {
-      const result = await fetchV2(this._mailSvc.URL, { ...this._mailSvc.headers, ...customHeaders });
-      return result[this._mailSvc.pathResponse];
+      const result = await fetchV2(this._mailSvc.URL, { ...this._mailSvc.headers, ...(customHeaders as OutgoingHttpHeaders) });
+      return result[this._mailSvc.pathResponse] ?? { email: null };
     }
+    return { email: null };
   }
 
-  private async useHeadless() {
+  /**
+   * Get the content of the email received from email address.
+   * 
+   * @notice This function currently only supports headless services and gets the subject of the email.
+   *         Full content fetching is not implemented yet.
+   * @param cookie The cookie data to maintain session state.
+   * @param from The from email address to check for new emails.
+   */
+  async getContent(cookie: CookieData[], from: string): Promise<ContentMailRes> {
+    if (this._mailSvc.headless) return this.getContentHeadless(cookie, from);
+    if (this._mailSvc.httpVersion === '2') {
+      throw new Error('Not supported yet for http2');
+    }
+    return { subject: null, content: null };
+  }
+
+  private async useHeadless(cookie?: CookieData[]): Promise<MailRes> {
     const headless = Headless.getInstance();
     const browser = await headless.open();
+    if (cookie && cookie.length) {
+      browser.setCookie(...cookie);
+    }
     const page = await browser.newPage();
     await page.goto(this._mailSvc.URL, {
       waitUntil: 'domcontentloaded'
     });
     try {
       await page.waitForSelector('#fe_text');
-      return await page.$eval('#fe_text', (el: any) => el.value ?? null);
+      const email = await page.$eval('#fe_text', (el: any) => el.value ?? null);
+      const cookie = await browser.cookies();
+      return { email, cookie };
     } catch (e) {
       console.error('Error fetching email:', e);
-      return null;
+      return { email: null };
     } finally {
       await browser.close();
     }
-    
+
+  }
+
+  private async getContentHeadless(cookie: CookieData[], email: string): Promise<ContentMailRes> {
+    return new Promise(async (resolve, reject) => {
+      const headless = Headless.getInstance();
+      const browser = await headless.open();
+      if (cookie && cookie.length) {
+        browser.setCookie(...cookie);
+      }
+      const page = await browser.newPage();
+      await page.goto(this._mailSvc.URL, {
+        waitUntil: 'domcontentloaded'
+      });
+      try {
+        console.debug("Checking email for:", email);
+        await page.waitForSelector('#fe_text');
+        let subject = "";
+        const interval = setInterval(async () => {
+          subject = await page.evaluate((email) => {
+            let result = "";
+            document.querySelectorAll('#maillist tr').forEach((el) => {
+              console.debug(el);
+              const from = el.querySelector('td:first-child')?.textContent ?? '';
+              console.debug('From:', from);
+              if (from.includes(email)) {
+                result = el.querySelector('td:nth-child(2)')?.textContent ?? '';
+                alert(result);
+              }
+            });
+            return result;
+          }, email);
+          if (subject) {
+            clearInterval(interval);
+            resolve({ content: null, subject, cookie: await browser.cookies() });
+            await browser.close();
+          }
+        }, 10000);
+
+        setTimeout(async () => {
+          clearInterval(interval);
+          resolve({ content: null, subject: null });
+          await browser.close();
+        }, 10 * 60 * 1000); // Timeout after 10 minutes
+      } catch (e) {
+        console.error('Error fetching email:', e);
+        resolve({ content: null, subject: null });
+        await browser.close();
+      }
+    });
+
+
   }
 
   getServices() {
